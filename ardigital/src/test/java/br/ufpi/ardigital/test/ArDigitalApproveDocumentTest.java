b2aade73257199f5bdd45f5b53aacc8b98fd5ba2
Ronyerison <ronyerisonbraga@gmail.com>
2016-07-08 17:21:13 -0300

    Inclusão de um teste para rejeição de documentos.

--- ardigital/src/test/java/br/ufpi/ardigital/test/ArDigitalApproveDocumentTest.java
+++ ardigital/src/test/java/br/ufpi/ardigital/test/ArDigitalApproveDocumentTest.java
@@ -18,7 +18,7 @@ import br.ufpi.ardigital.model.FileAr;
 import br.ufpi.ardigital.model.User;
 import br.ufpi.ardigital.util.Constant;
 
-public class ArDigitalAcceptDocumentTest {
+public class ArDigitalApproveDocumentTest {
 	private WebDriver driver;
 	private StringBuffer verificationErrors = new StringBuffer();
 
@@ -36,14 +36,51 @@ public class ArDigitalAcceptDocumentTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void sendDocumentValidTest() throws Exception {
+	public void acceptDocumentValidTest() throws Exception {
 		login(UserFactory.criaUsuarioValido());
 		sendDocument();
 		logout();
 		login(UserFactory.criaUsuarioAdminValido());
 		acceptDocument();
 	}
+	
+	/**
+	 * Este teste realiza o envio de um documento com um usuario normal e
+	 * realiza a rejeição do envio com um usuario admin
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void rejectDocumentValidTest() throws Exception {
+		login(UserFactory.criaUsuarioValido());
+		sendDocument();
+		logout();
+		login(UserFactory.criaUsuarioAdminValido());
+		rejectDocument();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
+	public void rejectDocument() throws Exception {
+		waitForLoadPage();
+	    driver.findElement(By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[13]/a/span")).click();
+	    waitForLoadPage();
+	    driver.findElement(By.id("formTabelaDocumentos:tabelaAprovacaoPendencia:0:rejeitarEnvio")).click();
+	    waitForLoadPage();
+	    driver.findElement(By.id("j_idt164:motivoRejeicaoEnvio")).clear();
+	    driver.findElement(By.id("j_idt164:motivoRejeicaoEnvio")).sendKeys("Rejeição Teste");
+	    driver.findElement(By.id("j_idt164:j_idt168")).click();
+	    waitForLoadPage();
+	    assertEquals("O envio do documento foi rejeitado com sucesso.", driver.findElement(By.cssSelector("span.ui-messages-info-summary")).getText());
+	  }
+	
 	private void acceptDocument() throws InterruptedException {
 		waitForLoadPage();
 		driver.findElement(
@@ -53,7 +90,9 @@ public class ArDigitalAcceptDocumentTest {
 		driver.findElement(
 				By.id("formTabelaDocumentos:tabelaAprovacaoPendencia:0:aprovarEnvio"))
 				.click();
+		waitForLoadPage();
 		driver.findElement(By.id("aprovarEnvioDialogForm:j_idt147")).click();
+		waitForLoadPage();
 		assertEquals(
 				"Documento aprovado com sucesso.",
 				driver.findElement(
@@ -126,13 +165,6 @@ public class ArDigitalAcceptDocumentTest {
 		Thread.sleep(2000);
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			fail(verificationErrorString);
-		}
-	}
+	
 
 }
