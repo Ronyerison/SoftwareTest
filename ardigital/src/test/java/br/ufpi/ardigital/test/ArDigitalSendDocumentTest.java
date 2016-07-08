2054573755003b9831795484e267c0ab5cb0d82a
Ronyerison <ronyerisonbraga@gmail.com>
2016-07-08 14:09:34 -0300

    Inclusão de novos testes para a funcionalidade de enviar documento.

--- ardigital/src/test/java/br/ufpi/ardigital/test/ArDigitalSendDocumentTest.java
+++ ardigital/src/test/java/br/ufpi/ardigital/test/ArDigitalSendDocumentTest.java
@@ -1,7 +1,6 @@
 package br.ufpi.ardigital.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +8,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -24,28 +24,20 @@ public class ArDigitalSendDocumentTest {
 
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty(Constant.ChromeDriverLib,
-				Constant.ChromeDriverPath);
+		System.setProperty(Constant.ChromeDriverLib, Constant.ChromeDriverPath);
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	/**
-	 * Teste para envio de um documento válido
-	 * //TODO: NESSE CASO DE TESTE É NECESSÁRIO ALTERAR O DIRETORIO DO ARQUIVO NA CLASSE FileArFactory;
+	 * Teste para envio de um documento válido //TODO: NESSE CASO DE TESTE É
+	 * NECESSÁRIO ALTERAR O DIRETORIO DO ARQUIVO NA CLASSE FileArFactory;
+	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void sendDocumentValidTest() throws Exception {
-		User validUser = UserFactory.criaUsuarioValido();
-		driver.get(Constant.BaseURL + Constant.LoginURL);
-		driver.findElement(By.id("j_idt11:email")).clear();
-		driver.findElement(By.id("j_idt11:email")).sendKeys(
-				validUser.getLogin());
-		driver.findElement(By.id("j_idt11:senha")).clear();
-		driver.findElement(By.id("j_idt11:senha")).sendKeys(
-				validUser.getPassword());
-		driver.findElement(By.id("j_idt11:j_idt17")).click();
+		login();
 		driver.findElement(
 				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
 				.click();
@@ -86,15 +78,7 @@ public class ArDigitalSendDocumentTest {
 	 */
 	@Test
 	public void sendDocumentInvalidTest() throws Exception {
-		User validUser = UserFactory.criaUsuarioValido();
-		driver.get(Constant.BaseURL + Constant.LoginURL);
-		driver.findElement(By.id("j_idt11:email")).clear();
-		driver.findElement(By.id("j_idt11:email")).sendKeys(
-				validUser.getLogin());
-		driver.findElement(By.id("j_idt11:senha")).clear();
-		driver.findElement(By.id("j_idt11:senha")).sendKeys(
-				validUser.getPassword());
-		driver.findElement(By.id("j_idt11:j_idt17")).click();
+		login();
 		driver.findElement(
 				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
 				.click();
@@ -119,15 +103,7 @@ public class ArDigitalSendDocumentTest {
 	 */
 	@Test
 	public void sendDocumentInvalidReceiverRegisterTest() throws Exception {
-		User validUser = UserFactory.criaUsuarioValido();
-		driver.get(Constant.BaseURL + Constant.LoginURL);
-		driver.findElement(By.id("j_idt11:email")).clear();
-		driver.findElement(By.id("j_idt11:email")).sendKeys(
-				validUser.getLogin());
-		driver.findElement(By.id("j_idt11:senha")).clear();
-		driver.findElement(By.id("j_idt11:senha")).sendKeys(
-				validUser.getPassword());
-		driver.findElement(By.id("j_idt11:j_idt17")).click();
+		login();
 		driver.findElement(
 				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
 				.click();
@@ -149,6 +125,116 @@ public class ArDigitalSendDocumentTest {
 						.getText());
 	}
 
+	/**
+	 * Teste para envio de um documento enviando os anexos para o e-carta com
+	 * numero de páginas acima do limite
+	 * @throws Exception
+	 */
+	@Test
+	public void ArDigitalSendDocumentInvalidPagesLimitTest() throws Exception {
+		login();
+		driver.findElement(
+				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
+				.click();
+		driver.findElement(By.id("form:conteudo")).clear();
+		driver.findElement(By.id("form:conteudo")).sendKeys(
+				"Documento com páginas acima do limite.");
+		driver.findElement(By.id("form:tipo_label")).click();
+		driver.findElement(By.id("form:tipo_1")).click();
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		driver.findElement(By.id("form:gestorAutocomplete_input")).clear();
+		driver.findElement(By.id("form:gestorAutocomplete_input")).sendKeys(
+				"SAULO DE TÁRSIO");
+		waitForLoadPage();
+		driver.findElement(
+				By.xpath("//div[@id='form:gestorAutocomplete_panel']/ul/li"))
+				.click();
+		waitForLoadPage();
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		waitForLoadPage();
+		driver.findElement(By.id("form:upOficio_input")).clear();
+		driver.findElement(By.id("form:upOficio_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:checkboxEnviarAnexos")).click();
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		assertTrue(isElementPresent(By
+				.cssSelector("span.ui-messages-error-summary")));
+	}
+
+	/**
+	 * Teste para envio de um documento enviando sem os anexos para o e-carta com
+	 * numero de páginas acima do limite
+	 * @throws Exception
+	 */
+	@Test
+	public void ArDigitalSendDocumentValidPagesLimitTest() throws Exception {
+		login();
+		driver.findElement(
+				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
+				.click();
+		driver.findElement(By.id("form:conteudo")).clear();
+		driver.findElement(By.id("form:conteudo")).sendKeys(
+				"Documento com páginas acima do limite.");
+		driver.findElement(By.id("form:tipo_label")).click();
+		driver.findElement(By.id("form:tipo_1")).click();
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		driver.findElement(By.id("form:gestorAutocomplete_input")).clear();
+		driver.findElement(By.id("form:gestorAutocomplete_input")).sendKeys(
+				"SAULO DE TÁRSIO");
+		waitForLoadPage();
+		driver.findElement(
+				By.xpath("//div[@id='form:gestorAutocomplete_panel']/ul/li"))
+				.click();
+		waitForLoadPage();
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		waitForLoadPage();
+		driver.findElement(By.id("form:upOficio_input")).clear();
+		driver.findElement(By.id("form:upOficio_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt117_input")).clear();
+		driver.findElement(By.id("form:j_idt117_input")).sendKeys(
+				Constant.FilePath);
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		waitForLoadPage();
+		driver.findElement(By.id("form:ajax")).click();
+		assertEquals("Documento encaminhado com sucesso!",
+				driver.findElement(By.cssSelector("div.ui-grid-col-12"))
+						.getText());
+	}
+	
 	@After
 	public void tearDown() throws Exception {
 		driver.quit();
@@ -158,8 +244,29 @@ public class ArDigitalSendDocumentTest {
 		}
 	}
 
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
 	private void waitForLoadPage() throws InterruptedException {
 		Thread.sleep(2000);
+	}
+
+	private void login() {
+		User validUser = UserFactory.criaUsuarioValido();
+		driver.get(Constant.BaseURL + Constant.LoginURL);
+		driver.findElement(By.id("j_idt11:email")).clear();
+		driver.findElement(By.id("j_idt11:email")).sendKeys(
+				validUser.getLogin());
+		driver.findElement(By.id("j_idt11:senha")).clear();
+		driver.findElement(By.id("j_idt11:senha")).sendKeys(
+				validUser.getPassword());
+		driver.findElement(By.id("j_idt11:j_idt17")).click();
 	}
 
 }
