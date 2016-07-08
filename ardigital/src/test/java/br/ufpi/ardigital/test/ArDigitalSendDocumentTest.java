a4fadf6d49e2cb3c99a6e67f3b4e373e3fde1eac
Ronyerison <ronyerisonbraga@gmail.com>
2016-07-08 10:51:40 -0300

    Merge nas classes de teste

--- ardigital/src/test/java/br/ufpi/ardigital/test/ArDigitalSendDocumentTest.java
+++ ardigital/src/test/java/br/ufpi/ardigital/test/ArDigitalSendDocumentTest.java
@@ -16,48 +16,25 @@ import br.ufpi.ardigital.factory.FileArFactory;
 import br.ufpi.ardigital.factory.UserFactory;
 import br.ufpi.ardigital.model.FileAr;
 import br.ufpi.ardigital.model.User;
+import br.ufpi.ardigital.util.Constant;
 
 public class ArDigitalSendDocumentTest {
 	private WebDriver driver;
-	private String baseUrl;
 	private StringBuffer verificationErrors = new StringBuffer();
 
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("chromedriver.exe",
-				"..//resources//chromedriver.exe");
+		System.setProperty(Constant.ChromeDriverLib,
+				Constant.ChromeDriverPath);
 		driver = new ChromeDriver();
-		baseUrl = "http://10.28.14.224:8181/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	@Test
-	public void sendDocumentInvalidTest() throws Exception {
-		User validUser = UserFactory.criaUsuarioValido();
-		driver.get(baseUrl + "/ar-digital/login.xhtml");
-		driver.findElement(By.id("j_idt11:email")).clear();
-		driver.findElement(By.id("j_idt11:email")).sendKeys(
-				validUser.getLogin());
-		driver.findElement(By.id("j_idt11:senha")).clear();
-		driver.findElement(By.id("j_idt11:senha")).sendKeys(
-				validUser.getPassword());
-		driver.findElement(By.id("j_idt11:j_idt17")).click();
-		driver.findElement(
-				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
-				.click();
-		driver.findElement(By.id("form:tipo_label")).click();
-		driver.findElement(By.id("form:tipo_1")).click();
-		driver.findElement(By.id("form:conteudo")).clear();
-		driver.findElement(By.id("form:conteudo")).sendKeys("Cobrança teste");
-		driver.findElement(By.id("form:j_idt49_next")).click();
-		waitForLoadPage();
-		driver.findElement(By.id("form:j_idt49_next")).click();
-		waitForLoadPage();
-		assertEquals("É necessário informar um Interessado", driver
-				.findElement(By.cssSelector("span.ui-messages-error-summary"))
-				.getText());
-	}
-
+	/**
+	 * Teste para envio de um documento válido
+	 * //TODO: NESSE CASO DE TESTE É NECESSÁRIO ALTERAR O DIRETORIO DO ARQUIVO NA CLASSE FileArFactory;
+	 * @throws Exception
+	 */
 	@Test
 	public void sendDocumentValidTest() throws Exception {
 		User validUser = UserFactory.criaUsuarioValido();
@@ -91,11 +68,85 @@ public class ArDigitalSendDocumentTest {
 		waitForLoadPage();
 		driver.findElement(By.id("form:upOficio_input")).clear();
 		FileAr file = FileArFactory.createValidFile();
-		driver.findElement(By.id("form:upOficio_input")).sendKeys(file.getPath());
+		driver.findElement(By.id("form:upOficio_input")).sendKeys(
+				file.getPath());
+		waitForLoadPage();
 		driver.findElement(By.id("form:j_idt49_next")).click();
 		waitForLoadPage();
 		driver.findElement(By.id("form:ajax")).click();
-	    assertEquals("Documento encaminhado com sucesso!", driver.findElement(By.cssSelector("div.ui-grid-col-12")).getText());
+		assertEquals("Documento encaminhado com sucesso!",
+				driver.findElement(By.cssSelector("div.ui-grid-col-12"))
+						.getText());
+	}
+
+	/**
+	 * Teste para envio de um documento sem selecionar o interessado.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void sendDocumentInvalidTest() throws Exception {
+		User validUser = UserFactory.criaUsuarioValido();
+		driver.get(Constant.BaseURL + Constant.LoginURL);
+		driver.findElement(By.id("j_idt11:email")).clear();
+		driver.findElement(By.id("j_idt11:email")).sendKeys(
+				validUser.getLogin());
+		driver.findElement(By.id("j_idt11:senha")).clear();
+		driver.findElement(By.id("j_idt11:senha")).sendKeys(
+				validUser.getPassword());
+		driver.findElement(By.id("j_idt11:j_idt17")).click();
+		driver.findElement(
+				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
+				.click();
+		driver.findElement(By.id("form:tipo_label")).click();
+		driver.findElement(By.id("form:tipo_1")).click();
+		driver.findElement(By.id("form:conteudo")).clear();
+		driver.findElement(By.id("form:conteudo")).sendKeys("Cobrança teste");
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		waitForLoadPage();
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		waitForLoadPage();
+		assertEquals("É necessário informar um Interessado", driver
+				.findElement(By.cssSelector("span.ui-messages-error-summary"))
+				.getText());
+	}
+
+	/**
+	 * Teste para envio de um documento inserindo um novo cadastro de
+	 * destinatario com campos em branco
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void sendDocumentInvalidReceiverRegisterTest() throws Exception {
+		User validUser = UserFactory.criaUsuarioValido();
+		driver.get(Constant.BaseURL + Constant.LoginURL);
+		driver.findElement(By.id("j_idt11:email")).clear();
+		driver.findElement(By.id("j_idt11:email")).sendKeys(
+				validUser.getLogin());
+		driver.findElement(By.id("j_idt11:senha")).clear();
+		driver.findElement(By.id("j_idt11:senha")).sendKeys(
+				validUser.getPassword());
+		driver.findElement(By.id("j_idt11:j_idt17")).click();
+		driver.findElement(
+				By.xpath("//div[@id='j_idt14:j_idt15']/ul/li[4]/a/span"))
+				.click();
+		driver.findElement(By.xpath("//div[@id='form:tipo']/div[3]")).click();
+		driver.findElement(By.id("form:tipo_1")).click();
+		driver.findElement(By.id("form:conteudo")).clear();
+		driver.findElement(By.id("form:conteudo")).sendKeys(
+				"Teste cadastro usuario invalido.");
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		driver.findElement(
+				By.cssSelector("div.ui-inputswitch-handle.ui-state-default"))
+				.click();
+		waitForLoadPage();
+		driver.findElement(By.id("form:j_idt49_next")).click();
+		assertEquals(
+				"É necessário inserir o bairro do endereço",
+				driver.findElement(
+						By.xpath("//div[@id='form:camposInteressadoMessage']/div/ul/li[5]/span"))
+						.getText());
 	}
 
 	@After
