f5074e10b38525cd0fdb945735ca73fe5356f58c
Ronyerison <ronyerisonbraga@gmail.com>
2016-07-11 15:06:11 -0300

    Atualização na classe de configurações do projeto.

--- ardigital/src/test/java/br/ufpi/ardigital/util/Config.java
+++ ardigital/src/test/java/br/ufpi/ardigital/util/Config.java
@@ -1,21 +1,24 @@
 package br.ufpi.ardigital.util;
 
 public class Config {
-	public static final String FILE_PATH = "/home/rafael/git/SoftwareTest/ardigital/src/test/resources/oficio_1_pag.pdf";
+	//Diretorio Arquivo Rafael
+	//public static final String FILE_PATH = "/home/rafael/git/SoftwareTest/ardigital/src/test/resources/oficio_1_pag.pdf";
+	//Diretorio Arquivo Rony
+	public static final String FILE_PATH = "C://Users//Ronyerison//git//SoftwareTest//ardigital//src//test//resources//oficio_1_pag.pdf";
 	
-	public static final String CHROME_DRIVER_PATH = "src//test//resources//chromedriver";
-//	public static final String CHROME_DRIVER_PATH = "src//test//resources//chromedriver.exe";
-//	public static final String CHROME_DRIVER_PATH = "..//resources//chromedriver.exe";
+	//Linux
+	//public static final String CHROME_DRIVER_PATH = "src//test//resources//chromedriver";
+	//Windows
+	public static final String CHROME_DRIVER_PATH = "src//test//resources//chromedriver.exe";
 	
 	public static final String CHROME_DRIVER_LIB = "webdriver.chrome.driver";
-//	public static final String CHROME_DRIVER_LIB = "chromedriver.exe";
 	
 	public static final String BASE_URL = "http://10.28.14.224:8181/";
 	public static final String LOGIN_URL = "/ar-digital/login.xhtml";
 	public static final String DOWNLOAD_URL = "/downloadDocumento/downloadDocumento.xhtml";
 	
-	public static final String COMMON_USER_NAME = "ar_user2";
+	public static final String COMMON_USER_NAME = "ar_user1";
 	public static final String COMMON_USER_PASSWORD = "armestrado2016";
-	public static final String ADMINISTRATOR_USER_NAME = "ar_user7";
+	public static final String ADMINISTRATOR_USER_NAME = "ar_user6";
 	public static final String ADMINISTRATOR_USER_PASSWORD = "armestrado2016";
 }
